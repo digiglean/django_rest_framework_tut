2e4402ea372fbc06ce4f896934def57a5be248a9
Chris <chris@CGB-iMac-2TSSD.local>
2020-11-30 13:10:00 -0700

    Initial Commit

---
+++ Fedora/Fedora/urls.py
@@ -0,0 +1,8 @@
+# Fedora/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('people/', include("people.urls")),
+]

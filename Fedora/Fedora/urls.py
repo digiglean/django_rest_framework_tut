e0ded3fb80761a55fe3edea0ca4d0bd1958d42dc
Chris <chris@CGB-iMac-2TSSD.local>
2020-11-30 13:54:26 -0700

    Finished Lesson 4

--- Fedora/Fedora/urls.py
+++ Fedora/Fedora/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('people/', include("people.urls")),
+    path('artifacts/', include("artifacts.urls")),
 ]
---
+++ Fedora/artifacts/models.py
@@ -0,0 +1,6 @@
+# artifacts/models.py
+from django.db import models
+
+class Artifact(models.Model):
+    name = models.CharField(max_length=100)
+    shiny = models.BooleanField()
